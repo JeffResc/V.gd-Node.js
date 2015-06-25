52ae8125fe446965e63f4997223d91cb52217b61
AlphaTech <alphatech@btri.x10.bz>
2015-06-24 23:46:00 -0400

    Fix URL Lookup

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ module.exports = {
         request('http://v.gd/create.php?format=simple&url=' + url, function (error, response, body) {
 			cb(body.split("\n")[0]);
         });
-    }
+    },
     lookup: function(url, cb) {
         request('http://v.gd/forward.php?shorturl=' + url, function (error, response, body) {
 			cb(body.split("\n")[0]);
