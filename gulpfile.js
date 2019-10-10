b39c1847052fb70022a3ed9c92a96693ddf52526
Papu Kumar(ReX) <papuruth@gmail.com>
2019-10-10 16:35:50 +0530

    Update gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -2,18 +2,17 @@ var gulp = require('gulp');
 var jshint = require('gulp-jshint');
 var markdown = require('gulp-markdown');
 
-gulp.task('default', (done) {
-  gulp.src('./*.js')
+gulp.task('default', async function() {
+  await gulp.src('./*.js')
     .pipe(jshint())
     .pipe(jshint.reporter('default'));
 
-  gulp.src('./*.md')
+  await gulp.src('./*.md')
     .pipe(markdown())
     .pipe(gulp.dest('./build/markdown'));
-  done();
 });
 
-gulp.task('watch', function () {
-    gulp.watch('./*.js', ['default']);
-    gulp.watch('./*.md', ['default']);
+gulp.task('watch', async function() {
+    await gulp.watch('./*.js', ['default']);
+    await gulp.watch('./*.md', ['default']);
 });
