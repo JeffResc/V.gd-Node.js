8cc55ec468c2b5010878a832c02786cfd7cacb3e
Papu Kumar(ReX) <papuruth@gmail.com>
2019-10-10 16:46:32 +0530

    Update gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -1,18 +1,27 @@
-var gulp = require('gulp'); 
+var gulp = require('gulp');
 var jshint = require('gulp-jshint');
 var markdown = require('gulp-markdown');
 
-gulp.task('default', async function() {
-  await gulp.src('./*.js')
-    .pipe(jshint())
-    .pipe(jshint.reporter('default'));
-
-  await gulp.src('./*.md')
-    .pipe(markdown())
-    .pipe(gulp.dest('./build/markdown'));
+gulp.task('default', function () {
+  let promise = new Promise(function (resolve, reject) {
+    resolve(gulp.src('./*.js')
+      .pipe(jshint())
+      .pipe(jshint.reporter('default')));
+    resolve(
+      gulp.src('./*.md')
+        .pipe(markdown())
+        .pipe(gulp.dest('./build/markdown')));
+  });
+  promise
+    .then(function (result) {
+      return result;
+    })
+    .catch(function (error) {
+      return error
+    })
 });
 
-gulp.task('watch', async function() {
-    await gulp.watch('./*.js', ['default']);
-    await gulp.watch('./*.md', ['default']);
+gulp.task('watch', function () {
+  gulp.watch('./*.js', ['default']);
+  gulp.watch('./*.md', ['default']);
 });
